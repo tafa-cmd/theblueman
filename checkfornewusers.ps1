f73bd7898d4bade86d60c2a8ef540944335185b0
j-sullivan0 <72814202+j-sullivan0@users.noreply.github.com>
2020-10-14 15:48:38 -0400

    Add files via upload

---
+++ checkfornewusers.ps1
@@ -0,0 +1,2 @@
+﻿$When = ((Get-Date).AddMinutes(-6))
+Get-ADUser -Filter {whenCreated -ge $When} -Properties whenCreated | Out-File -FilePath C:\USER_LOGS\log.txt
---
+++ createscheduledtask.ps1
@@ -0,0 +1,12 @@
+﻿md C:\USER_LOGS
+md C:\scripts
+Copy-Item .\changedomainuserpw.ps1 -Destination C:\scripts
+Copy-Item .\checkfornewusers.ps1 -Destination C:\scripts
+cd C:\scripts
+#& .\changedomainuserpw.ps1
+
+$task = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-executionpolicy bypass -noprofile -file c:\scripts\checkfornewusers.ps1'
+$interval = New-TimeSpan -Minutes 5
+$starttime = (Get-Date).AddMinutes(1)
+$trigger = New-ScheduledTaskTrigger -Once -At $starttime -RepetitionInterval $interval
+Register-ScheduledTask -Action $task -Trigger $trigger -TaskName CheckForNewUsers
