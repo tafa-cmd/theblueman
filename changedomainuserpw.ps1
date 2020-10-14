f794408d4938bc5179cc141bff8f1fc43655121f
j-sullivan0 <72814202+j-sullivan0@users.noreply.github.com>
2020-10-14 13:57:34 -0400

    Add files via upload

---
+++ changedomainuserpw.ps1
@@ -0,0 +1,8 @@
+﻿$users = @("bruceherring", "daryljohnson", "billstackpole", "garretacoraci", "robolson", "justinpelletier", "chaimsanders", "boyuan", "giovaniabuatiah")
+$oldpass = "PleaseChange123!"
+$newpass = "jazzyPl@nt70"
+
+foreach ($user in $users) {
+    Set-ADAccountPassword -Identity $user -OldPassword (ConvertTo-SecureString -AsPlainText $oldpass -Force) -NewPassword (ConvertTo-SecureString -AsPlainText $newpass -Force)
+}
+
